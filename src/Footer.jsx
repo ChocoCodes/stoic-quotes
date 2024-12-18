c71c6a77e00c7bb11c195b4241f975e11ca09913
ChocoCodes <chocojhong999@gmail.com>
2024-12-18 21:41:49 +0800

    Fixed: Acknowledgement typo

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import './App.css';
 export default function Footer() {
     return (
         <div className="footer">
-            <p>Acknowledgement: u/tcheah2 for the <span><a className='ref-link' href="https://github.com/tlcheah2/stoic-quote-lambda-public-api?tab=readme-ov-file">API</a>.</span></p>
+            <p>Acknowledgement: u/tlcheah2 for the <span><a className='ref-link' href="https://github.com/tlcheah2/stoic-quote-lambda-public-api?tab=readme-ov-file">API</a>.</span></p>
             <p>Developed by <span><a className='ref-link' href="https://github.com/ChocoCodes">John Octavio</a></span> | 2024</p>
         </div>
     );
