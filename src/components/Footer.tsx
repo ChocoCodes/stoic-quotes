164675d839aea66f0cf6858b61fdafa741c2ab7a
ChocoCodes <chocojhong999@gmail.com>
2025-01-02 22:59:20 +0800

    Add: Mobile Resposiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ import '../styles/style.css';
 export default function Footer() {
     return (
         <div className="footer">
-            <p>Acknowledgement: u/tlcheah2 for the <span><a className='ref-link' href="https://github.com/tlcheah2/stoic-quote-lambda-public-api?tab=readme-ov-file">API</a>.</span></p>
-            <p>Developed by <span><a className='ref-link' href="https://github.com/ChocoCodes">John Octavio</a></span> | 2024</p>
+            <p>Acknowledgement: u/tlcheah2 for the <a className='ref-link' href="https://github.com/tlcheah2/stoic-quote-lambda-public-api?tab=readme-ov-file">API</a>.</p>
+            <p>Developed by <a className='ref-link' href="https://github.com/ChocoCodes">John Octavio</a> | 2024</p>
         </div>
     );
 }
